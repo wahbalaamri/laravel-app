070f8cdcd82262063bf57d9ea547686b53cc43e6
wahbalaamri <wahb.alaamri@gmail.com>
2019-08-22 13:28:55 +0400

    add auto hr

---
+++ laravel/resources/views/answers/_answer.blade.php
@@ -0,0 +1,37 @@
+<div class="media post">
+    @include('shared._vote',[
+    'model'=> $answer,
+    ])
+
+<div class="media-body">
+    {!! $answer->body_html !!}
+    <div class="row">
+        <div class="col-4">
+            <div class="ml-auto">
+                {{-- @if (Auth::user()->can('update-question',$answer)) from lesson-12-a --}}
+                @can('update',$answer)
+                <a href="{{ route('questions.answers.edit', [$question->id,$answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                {{-- @endif --}}
+                @endcan
+                {{-- @if (Auth::user()->can('update-question',$answer)) from lesson-12-a--}}
+                @can('delete',$answer)
+                <form action="{{ route('questions.answers.destroy',[$question->id,$answer->id]) }}" class="form-delete" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure You Want To Delete This Answer?')">Delete</button>
+                </form>
+                @endcan
+                {{-- @endif --}}
+
+            </div>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4">
+            @include('shared._author',[
+            'model' => $answer,
+            'label'=>'Answered'
+            ])
+        </div>
+    </div>
+
+</div>
